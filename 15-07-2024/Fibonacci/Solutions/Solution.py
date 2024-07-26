cf62de0ef0d450f9789ab682ec6582aed5a5d21b
Prabhat Jana Official <89354282+iamthetwodigiter@users.noreply.github.com>
2024-07-26 19:32:53 +0530

    Added an alternative solution

--- 15-07-2024/Fibonacci/Solutions/Solution.py
+++ 15-07-2024/Fibonacci/Solutions/Solution.py
@@ -3,3 +3,12 @@ class Solution:
         if n < 2:
             return n
         return self.fib(n - 1) + self.fib(n - 2)
+
+# Added an alternative solution
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if(n >= 2):
+            return self.fib(n-1) + self.fib(n-2)
+        return n
+        
