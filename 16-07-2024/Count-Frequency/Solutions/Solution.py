a581229d33ba47d87c9f30e33a45447fa2e1ffb1
Prabhat Jana Official <89354282+iamthetwodigiter@users.noreply.github.com>
2024-07-26 19:40:16 +0530

    Added an alternative solution

--- 16-07-2024/Count-Frequency/Solutions/Solution.py
+++ 16-07-2024/Count-Frequency/Solutions/Solution.py
@@ -14,3 +14,21 @@ def frequencyCount(self, arr, N, P):
     # Copy the frequencies back to the original array
     for i in range(N):
         arr[i] = ans[i]
+
+
+# Alternate Solution
+
+class Solution:
+    def frequencyCount(self, arr, N, P):
+        hash_map = {}
+        for num in arr:
+            if num in hash_map:
+                hash_map[num] +=1
+            else:
+                hash_map[num] = 1
+        
+        for i in range(1, N+1):
+            if i in hash_map:
+                arr[i-1] = hash_map[i]
+            else:
+                arr[i-1] = 0
