bc2bbfa1263640c72a6302523614d96a892ba113
Prabhat Jana Official <89354282+iamthetwodigiter@users.noreply.github.com>
2024-07-26 20:01:28 +0530

    Added an alternative solution

--- 22-07-2024/missing-in-array/solutions/solution.py
+++ 22-07-2024/missing-in-array/solutions/solution.py
@@ -12,3 +12,11 @@ class Solution:
             sum += _
         
         return (int)( n * (n + 1) / 2) - sum
+
+# Alternate Solution
+class Solution:
+    def missingNumber(self, n, arr):
+        # this is really a tempting solution, and to go for many codes as this is that comes to the mind first, but is not optimised
+        # return ([x for x in range(1, n+1) if x not in arr][0])
+        
+        return int(((n)*(n+1))/2 - sum(arr))
