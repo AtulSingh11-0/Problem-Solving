00c1c07ca52f68e106c87d44107b3dc75be633b9
Atul Singh <cocatul11@gmail.com>
2024-07-28 14:07:43 +0530

    feat: Optimize anagram solutions

--- 27-07-2024/anagram/solutions/solution.java
+++ 27-07-2024/anagram/solutions/solution.java
@@ -1,6 +1,7 @@
 class Solution
 {    
     //Function is to check whether two strings are anagram of each other or not.
+    // optimized : atul
     public static boolean isAnagram(String a,String b)
     {
         // Your code here
@@ -26,5 +27,52 @@ class Solution
                 return false;
         }
         return true;
+    }
+
+    // optimized : soumyadeep
+    public static boolean isAnagram(String a,String b)
+    {
+        
+        // Your code here
+        char [] arr1=a.toCharArray();
+        char [] arr2=b.toCharArray();
+        int [] frequency1=new int [26];
+        int [] frequency2=new int [26];
+        for( char i:arr1)
+        {
+            int ind=i-'a';
+            frequency1[ind]++;
+        }
+        for( char i:arr2)
+        {
+            int ind=i-'a';
+            frequency2[ind]++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(frequency2[i]!=frequency1[i])
+                return false;
+                
+        }
+        return true;
+        
+        
+    }
+
+    // optimized : debosmita
+    public static boolean isAnagram(String a,String b)
+    {
+        
+    if (a.length() != b.length()) {
+            return false;
+        }
+        
+        char[] charArrayA = a.toCharArray();
+        char[] charArrayB = b.toCharArray();
+        
+        Arrays.sort(charArrayA);
+        Arrays.sort(charArrayB);
+        
+        return Arrays.equals(charArrayA, charArrayB);
     }
 }
--- 27-07-2024/remove-duplicates/solutions/solution.java
+++ 27-07-2024/remove-duplicates/solutions/solution.java
@@ -1,18 +1,53 @@
 public class solution {
+
+  // optimized : soumyadeep
   String removeDups(String str) {
     // code here
 
-  String ans="";
-  char [] arr1=str.toCharArray();
-  boolean [] present=new boolean[26];
-  for( char i:arr1)
-    {
-        int ind=i-'a';
-        if(present[ind]==false){
-            present[ind]=true;
-            ans=ans+i;
-        }      
+    String ans="";
+    char [] arr1=str.toCharArray();
+    boolean [] present=new boolean[26];
+    for( char i:arr1)
+      {
+          int ind=i-'a';
+          if(present[ind]==false){
+              present[ind]=true;
+              ans=ans+i;
+          }      
+      }
+    return ans;
+  }
+
+  // optimized : debosmita
+  String removeDups(String str) {
+    String S="";
+    for(int i=0;i<str.length();i++){
+        String s="";
+        s+=str.charAt(i);
+        
+        if(!S.contains(s)){
+        S+=str.charAt(i);
+        }
     }
-  return ans;
-}
+    return S;
+  }
+
+  // optimized : atul
+  String removeDups(String str) {
+    // code here
+    
+    StringBuilder sb = new StringBuilder();
+    HashSet<Character> set = new HashSet<>();
+    
+    for ( int i = 0; i < str.length(); i++ ) {
+        char ch = str.charAt(i);
+        
+        if ( set.add(ch) ) {
+            sb.append(ch);
+        }
+    }
+    
+    return sb.toString();
+    
+  }
 }
