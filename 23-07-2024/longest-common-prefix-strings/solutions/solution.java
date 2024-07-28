fc011e839b41b9316e6126222b64e7f1a0604446
Atul Singh <cocatul11@gmail.com>
2024-07-28 14:17:26 +0530

    Optimize longest common prefix solution

--- 23-07-2024/longest-common-prefix-strings/solutions/solution.java
+++ 23-07-2024/longest-common-prefix-strings/solutions/solution.java
@@ -1,3 +1,39 @@
 public class solution {
-  
+
+  // brute force : soumyadeep
+  public String longestCommonPrefix(String arr[]) {
+    String ans="";
+    int N=arr.length;
+    if(N==0)
+        return "-1";
+    String s = arr[0];
+    
+    int minl = arr[0].length();
+    for(int i = 0; i<N; i++){
+        minl = Math.min(minl, arr[i].length());
+    }
+    
+    for(int i = 0; i<N; i++){
+        arr[i] = arr[i].substring(0,minl);
+    }
+    
+    for(int i=0;i<minl;i++)
+    {
+        char c = arr[0].charAt(i);
+        for(int j=1;j<N;j++)
+        {
+            if(arr[j].charAt(i)==c)
+                  continue;
+            else
+            {
+                if(ans.length()==0)
+                    return "-1";
+                else
+                    return ans;
+            }
+        }
+        ans=ans+c;
+    }
+    return ans;   
+  }       
 }
